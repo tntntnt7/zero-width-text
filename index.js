3f2d75b66d70e84c0412ec5c881c2aa638ded57b
Niko <tntntnt7@gmail.com>
2020-12-22 00:35:49 +0800

    add babelrc

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const encode = content => [...[...content].map(cell => cell.charCodeAt().toString(2)).join(' ')]
-  .map(cell => {
-    if (cell === '0') return '\u200b'
-    if (cell === '1') return '\u200c'
-    return '\u200d'
-  })
-  .join('')
+const { encode, decode } = require('./lib/index')
 
-const decode = content => [...content].map(cell => {
-    if (cell === '\u200b') return 0
-    if (cell === '\u200c') return 1
-    return ' '
-  })
-  .join('')
-  .split(' ')
-  .map(cell => String.fromCharCode(parseInt(cell, 2)))
-  .join('')
-
-  export default {
-    encode,
-    decode,
-  }
-  
+module.exports = {
+  encode,
+  decode,
+}
